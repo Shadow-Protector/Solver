81e13295644cfc261ffbd540b788a59700120601
parizval <anmolgoyal@gmail.com>
2025-05-28 18:39:29 +0530

    created config file for multichain handlers

---
+++ config/contracts.ts
@@ -0,0 +1,6 @@
+import { arbitrum, base } from "viem/chains";
+
+export const HandlerContracts = {
+  [arbitrum.id.toString()]: "0x03c8c7845200484d4dfeac074e616a4e415c6294",
+  [base.id.toString()]: "0xdE8bb0fbcA6deE981c607C54f94bdd34A9D15362",
+};
